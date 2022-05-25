8003406005fc6e4fcf8b0c76a75d36dd951f71e6
tommy <thmorriss@gmail.com>
2022-05-25 17:19:49 -0400

    filter by search box

--- src/frontdsl/page.cljs
+++ src/frontdsl/page.cljs
@@ -3,10 +3,29 @@
             [reagent.dom :as rdom]
             [clojure.string :as str]))
 
-(defn tdsl-app [inp]
-  [:table 
-   (for [[kw thought] inp]
-     [:tr [:td.kw [:pre.swag (str kw)]] [:td [:pre thought]]])])
+(defonce todos (r/atom {}))
+(defonce filterbox (r/atom ""))
+
+(defn filter-input []
+  [:input {:type "text"
+           :value @filterbox
+           :on-change #(reset! filterbox (-> % .-target .-value))}])
+
+(def filter-input 
+  (with-meta filter-input
+    {:component-did-mount #(.focus (rdom/dom-node %))}))
+
+(defn tdsl-app []
+  [:div
+   [filter-input]
+   [:table 
+    [:tbody
+     (doall (for [[kw thought] (->> @todos
+                                    (filter #(if (not (empty? @filterbox))
+                                         (str/includes? (str (first %)) @filterbox)
+                                         (constantly true))))]
+        [:tr [:td.kw [:pre.swag (str kw)]] [:td [:pre thought]]]))]]])
 
 (defn ^:export run [inp]
-  (rdom/render [tdsl-app (js->clj inp)] (js/document.getElementById "app")))
+  (reset! todos (js->clj inp))
+  (rdom/render [tdsl-app] (js/document.getElementById "app")))
