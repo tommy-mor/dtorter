707a817608cd17ad159bc235d3370c497480b253
tommy <thmorriss@gmail.com>
2022-05-25 17:57:02 -0400

    added routing

--- src/frontdsl/page.cljs
+++ src/frontdsl/page.cljs
@@ -1,31 +1,50 @@
 (ns frontdsl.page
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [reitit.frontend :as rf]
+            [reitit.frontend.easy :as rfe]))
 
 (defonce todos (r/atom {}))
 (defonce filterbox (r/atom ""))
 
+(defonce match (r/atom nil))
+(defn query-from-match []
+  (-> @match
+      :query-params
+      :q))
+
 (defn filter-input []
   [:input {:type "text"
-           :value @filterbox
-           :on-change #(reset! filterbox (-> % .-target .-value))}])
+           :value (query-from-match)
+           :on-change #(rfe/push-state ::tdsl {} {:q (-> % .-target .-value)})}])
 
 (def filter-input 
   (with-meta filter-input
     {:component-did-mount #(.focus (rdom/dom-node %))}))
 
 (defn tdsl-app []
-  [:div
-   [filter-input]
-   [:table 
-    [:tbody
-     (doall (for [[kw thought] (->> @todos
-                                    (filter #(if (not (empty? @filterbox))
-                                         (str/includes? (str (first %)) @filterbox)
-                                         (constantly true))))]
-        [:tr [:td.kw [:pre.swag (str kw)]] [:td [:pre thought]]]))]]])
+  (let [q (query-from-match)]
+    [:div
+     [filter-input]
+     [:table 
+      [:tbody
+       (doall (for [[kw thought] (->> @todos
+                                      (filter #(if (not (empty? q))
+                                                 (str/includes? (str (first %)) q)
+                                                 (constantly true))))]
+                [:tr [:td.kw [:pre.swag (str kw)]] [:td [:pre thought]]]))]]]))
+
+(def routes
+  [["/"
+    {:name ::tdsl
+     :view tdsl-app
+     :parameters {:query {:q string?}}}]])
 
 (defn ^:export run [inp]
   (reset! todos (js->clj inp))
+  (rfe/start!
+   (rf/router routes)
+   (fn [m] (reset! match m))
+   {:use-fragment true})
   (rdom/render [tdsl-app] (js/document.getElementById "app")))
