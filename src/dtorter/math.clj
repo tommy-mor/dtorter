eafc22042959a6c84e5bff2738d465ff6992d1ef
tommy <thmorriss@gmail.com>
2022-06-11 18:01:00 -0400

    fixed math error

--- src/dtorter/math.clj
+++ src/dtorter/math.clj
@@ -124,90 +124,92 @@
   (def sorted sorted)
   (def id->item id->item)
   (def itemid->elo (into {} (map (juxt :id :elo) sorted)))
-  
-  ;; item->{votes}
-  (def itemid->votes (apply merge-with into
-                            (for [{left :left-item right :right-item :as vote}  filteredvotes]
-                              {left #{vote} right #{vote}})))
 
-  (defn vote-ratio [itemid votes]
-    (apply /
-           (reduce (fn [[numerator denominator] {:keys [left-item right-item magnitude]}]
-                     (let [edutingam (- 100 magnitude)]
-                       (condp = itemid
-                         left-item [(+ numerator edutingam)
-                                    (+ denominator magnitude)]
-                         right-item [(+ numerator magnitude)
-                                     (+ denominator edutingam)]))) [0 0] votes)))
-  ;; use include in ratio the amount of votes, prioritize ones with less.
-  ;; PROBLEM, the ones near top will have winning/unbalanced ratios...
-  ;; maybe this strategy only works well at the beginning.
-  (defn sample-size [denominator]
-    (max (int (/ (count voteditems) denominator))
-         1))
+  (if (and (empty? voteditems) (empty? unvoteditems))
+    {:left nil :right nil}
+    (do
+      
+      ;; item->{votes}
+      (def itemid->votes (apply merge-with into
+                                (for [{left :left-item right :right-item :as vote}  filteredvotes]
+                                  {left #{vote} right #{vote}})))
 
-  (def sorted-ratios
-    (into (pm/priority-map) (for [{itemid :id} (-> sorted vec (sample-coll (sample-size 2) (kixi.stats.distribution/beta {:a 2 :b 3.3})))]
-                              {itemid (let [x (vote-ratio itemid (itemid->votes itemid))]
-                                        (if (< x 1.0)
-                                          (Math/pow x -1.0)
-                                          (float x)))})))
-  
-  ;; todo make way to switch between these ors
-  (def leftitem (or
-                 ;; TODO make this remember the score, to give rightitem a relative direction?
-                 (when (empty? voteditems)
-                   (rand-nth unvoteditems))
-                 (-> (rand-nth (take (sample-size 3) (reverse sorted-ratios)))
-                     first
-                     id->item)
-                 (comment
-                   (id->item (some (fn [[itemid votecount]]
-                                     (when (< votecount 3) itemid))
-                                   itemvotecounts))
-                   (draw-coll voteditems (kixi.stats.distribution/beta {:a 2 :b 3.3})))))
-  
-  (def voted-pairs (->> filteredvotes
-                        (filter (fn [{:keys [left-item right-item]}]
-                                  (or (= left-item (:id leftitem))
-                                      (= right-item (:id leftitem)))))
-                        (map (fn [{:keys [left-item right-item]}]
-                               [left-item right-item]))
-                        flatten
-                        set))
-  
-  (def rightitem (or (draw-coll unvoteditems (kixi.stats.distribution/uniform 0 1))
-                     (->> (into (pm/priority-map)
-                                (for [item (filter #(voted-pairs (:id %)) sorted)]
-                                  {(:id item) (Math/abs (- (:elo item)
-                                                           (itemid->elo (:id leftitem))))}))
-                          (drop 1)
-                          (take (sample-size 12))
-                          rand-nth
-                          first
-                          id->item)))
+      (defn vote-ratio [itemid votes]
+        (apply /
+               (reduce (fn [[numerator denominator] {:keys [left-item right-item magnitude]}]
+                         (let [edutingam (- 100 magnitude)]
+                           (condp = itemid
+                             left-item [(+ numerator edutingam)
+                                        (+ denominator magnitude)]
+                             right-item [(+ numerator magnitude)
+                                         (+ denominator edutingam)]))) [0 0] votes)))
+      ;; use include in ratio the amount of votes, prioritize ones with less.
+      ;; PROBLEM, the ones near top will have winning/unbalanced ratios...
+      ;; maybe this strategy only works well at the beginning.
+      (defn sample-size [denominator]
+        (max (int (/ (count voteditems) denominator))
+             1))
 
-  ;; TODO make this do loop
-  (assert (not (= leftitem rightitem)))
-  ;; TODO match items who want losses with items who want wins
+      (def sorted-ratios
+        (into (pm/priority-map) (for [{itemid :id} (-> sorted vec (sample-coll (sample-size 2) (kixi.stats.distribution/beta {:a 2 :b 3.3})))]
+                                  {itemid (let [x (vote-ratio itemid (itemid->votes itemid))]
+                                            (if (< x 1.0)
+                                              (Math/pow x -1.0)
+                                              (float x)))})))
+      
+      ;; todo make way to switch between these ors
+      (def leftitem (or
+                     ;; TODO make this remember the score, to give rightitem a relative direction?
+                     (when (empty? voteditems)
+                       (rand-nth unvoteditems))
+                     (-> (rand-nth (take (sample-size 3) (reverse sorted-ratios)))
+                         first
+                         id->item)
+                     (comment
+                       (id->item (some (fn [[itemid votecount]]
+                                         (when (< votecount 3) itemid))
+                                       itemvotecounts))
+                       (draw-coll voteditems (kixi.stats.distribution/beta {:a 2 :b 3.3})))))
+      
+      (def voted-pairs (->> filteredvotes
+                            (filter (fn [{:keys [left-item right-item]}]
+                                      (or (= left-item (:id leftitem))
+                                          (= right-item (:id leftitem)))))
+                            (map (fn [{:keys [left-item right-item]}]
+                                   [left-item right-item]))
+                            flatten
+                            set))
+      
+      (def rightitem (or (draw-coll unvoteditems (kixi.stats.distribution/uniform 0 1))
+                         (->> (into (pm/priority-map)
+                                    (for [item (filter #(voted-pairs (:id %)) sorted)]
+                                      {(:id item) (Math/abs (- (:elo item)
+                                                               (itemid->elo (:id leftitem))))}))
+                              (drop 1)
+                              (take (sample-size 12))
+                              rand-nth
+                              first
+                              id->item)))
 
-  (if (contains? voted-pairs #{(:id leftitem) (:id rightitem)})
-    (do (println "we already found pair \n"
-                 leftitem
-                 "\n"
-                 rightitem
-                 "\n trying again..")
-        (getpair args ctx)))
+      ;; TODO make this do loop
+      (assert (not (= leftitem rightitem)))
+      ;; TODO match items who want losses with items who want wins
 
-  #_(simple pair chosing for beginning. if an item has no losses, elomtach
-          as soon as it gets a loss, move on.
-          )
-  
+      (if (contains? voted-pairs #{(:id leftitem) (:id rightitem)})
+        (do (println "we already found pair \n"
+                     leftitem
+                     "\n"
+                     rightitem
+                     "\n trying again..")
+            (getpair args ctx)))
 
-  ;; if less than 2 exists, find those. then do unequal ratio strategy
-  ;; 
-  {:left leftitem
-   :right rightitem}
+      ;; simple pair chosing for beginning. if an item has no losses, elomtach
+      ;; as soon as it gets a loss, move on.
+
+      ;; if less than 2 exists, find those. then do unequal ratio strategy
+      ;; 
+      {:left leftitem
+       :right rightitem}))
 
   ;; TODO even more heavy preference for items with no losing matchups...
 
