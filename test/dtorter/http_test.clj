8c8729c2e7fe3682a0dd0bb77fbff764eed7398c
tommy <thmorriss@gmail.com>
2022-06-23 16:14:10 -0400

    i don't know whats going on

--- test/dtorter/http_test.clj
+++ test/dtorter/http_test.clj
@@ -41,10 +41,16 @@
                 (martian/response-for m :tag/delete {:id t-id})))))
   (stop))
 
+(defn calc-score [a b]
+  (let [idx1 (str/index-of alphabet-string a)
+        idx2 (str/index-of alphabet-string b)]
+    (+ 50 (- idx2 idx1))))
+
 (deftest alphabet
   (reset)
   (def m (martian-http/bootstrap-openapi "http://localhost:8080/api/swagger.json"))
-  (testing "can sort the alphabet"
+  (def alphabet-string "abcdefghijklmnopqrstuvwxyz")
+  (testing "can sort the alphabet with relative weights"
     (def tag (-> (martian/response-for m :tag/new {:tag/name "TESTING alphabet"
                                                    :tag/description "for testing"
                                                    :owner tommy})
@@ -52,7 +58,7 @@
                  :xt/id))
 
     (def sent-ids (set (doall
-                        (for [x "abcdefghijklmnopqrstuvwxyz"]
+                        (for [x alphabet-string]
                           (-> (martian/response-for m :item/new {:item/name (str x) 
                                                                  :item/tags [tag]
                                                                  :owner tommy})
@@ -69,17 +75,18 @@
                   :pair))
     (def left (:left pair))
     (def right (:right pair))
-    (let [[winner loser] (sort-by :item/name [left right])]
+    (let [mag (calc-score (:item/name left)
+                          (:item/name right))]
       (is (= 201 (:status
-                  (martian/response-for m :vote/new {:vote/left-item (:xt/id winner)
-                                                     :vote/right-item (:xt/id loser)
-                                                     :vote/magnitude 20
+                  (martian/response-for m :vote/new {:vote/left-item (:xt/id left)
+                                                     :vote/right-item (:xt/id right)
+                                                     :vote/magnitude mag
                                                      :vote/attribute "good attribute"
                                                      :vote/tag tag
                                                      :owner tommy})))))
     
     (def sorted (-> (martian/response-for m :tag/sorted {:id tag})
-                  :body))
+                    :body))
 
     (is (= 2 (count (:voteditems sorted))))
     (is (= 2 (count (:sorted sorted))))
@@ -88,11 +95,11 @@
     (is (= (list ["good attribute" 1]) (sorted :frequencies)))
     (for [_ (range 100)]
       (let [{{:keys [left right]} :pair} (:body (martian/response-for m :tag/sorted {:id tag}))
-            [winner loser] (sort-by :item/name [left right])]
+            mag (calc-score (:item/name left) (:item/name right))]
         
-        (martian/response-for m :vote/new {:vote/left-item (:xt/id winner)
-                                           :vote/right-item (:xt/id loser)
-                                           :vote/magnitude 20
+        (martian/response-for m :vote/new {:vote/left-item (:xt/id left)
+                                           :vote/right-item (:xt/id right)
+                                           :vote/magnitude mag
                                            :vote/attribute "good attribute"
                                            :vote/tag tag
                                            :owner tommy})))
@@ -110,6 +117,13 @@
     ;; todo make tag/sorted thing
     )
   (stop))
+
+(deftest alphabet-bad
+  #_(reset)
+  (def m (martian-http/bootstrap-openapi "http://localhost:8080/api/swagger.json"))
+  (def alphabet-string "abcdefghijklmnopqrstuvwxyz")
+  (comment "TODO make it so that pair chosing is good enough to sort without relative weights")
+  #_(stop))
 
 (deftest illegal-item
   (reset)
