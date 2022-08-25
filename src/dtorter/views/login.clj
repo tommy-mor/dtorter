a024e404b4c4d4ca25d5c316f93b5e12823fd095
tommy <thmorriss@gmail.com>
2022-08-25 12:19:37 -0700

    finished registration

--- src/dtorter/views/login.clj
+++ src/dtorter/views/login.clj
@@ -11,23 +11,30 @@
             (assoc-in
              ctx
              [:response :html]
-             [:form {:action (c/rurl-for ctx :login)
-                     :method "POST"}
-              [:input {:required true :type "text" :name "username" :placeholder "user"}]
-              [:input {:required true :type "password" :name "password" :placeholder "pass"}]
-              [:input {:type "submit" :value "login"}]]))})
+             [:div
+              (if-let [error (:error ctx)]
+                [:b error])
+              [:form {:action (c/rurl-for ctx :login)
+                      :method "POST"}
+               [:input {:required true :type "text" :name "username" :placeholder "user"}]
+               [:input {:required true :type "password" :name "password" :placeholder "pass"}]
+               [:input {:type "submit" :value "login"}]]]))})
 
 (def register-page
   {:leave (fn [ctx]
             (assoc-in
              ctx
              [:response :html]
-             [:form {:action (c/rurl-for ctx :register)
-                     :method "POST"}
-              [:input {:required true :type "text" :name "username" :placeholder "user"}]
-              [:input {:required true :type "password" :name "password" :placeholder "pass"}]
-              [:input {:required true :type "password" :name "password-again" :placeholder "pass"}]
-              [:input {:type "submit" :value "register"}]]))})
+             [:div
+              (if-let [error (:error ctx)]
+                [:b error])
+              
+              [:form {:action (c/rurl-for ctx :register)
+                      :method "POST"}
+               [:input {:required true :type "text" :name "username" :placeholder "user"}]
+               [:input {:required true :type "password" :name "password" :placeholder "pass"}]
+               [:input {:required true :type "password" :name "password-again" :placeholder "pass"}]
+               [:input {:type "submit" :value "register"}]]]))})
 
 (def login-done
   {:name ::login-done
@@ -58,7 +65,9 @@
                                (assoc :session {:user-id (:xt/id user-doc)
                                                 :user-name (:user/name user-doc)}
                                       :session-cookie-attrs {:expires cookie-expires})))
-                    ((:leave login-page) ctx)))))})
+                    ((:leave login-page) (assoc ctx
+                                                :error
+                                                "bad login, try again"))))))})
 (def register-done
   {:name ::register-done
    :leave (fn [ctx]
@@ -74,27 +83,19 @@
                                                :where [e :user/name username]]
                                              (form "username"))))
             (if existing-user
-              (assoc ctx :response))
-            
-            
-            (-> (let [form (:form-params (:request ctx))
-                      username (form "username")
-                      password (form "password")
-                      user-doc (ffirst (xt/q (xt/db (-> ctx :request :node))
-                                             '[:find (pull e [*])
-                                               :in username
-                                               :where [e :user/name username]]
-                                             username))
-                      password-hash (:user/password-hash user-doc)
-                      
-                      
-                      ]
-                  (if (and password-hash (hashing/check-pw password password-hash))
-                    (assoc ctx :response
-                           (-> (ring-resp/redirect (c/rurl-for ctx :front-page))
-                               (assoc :session {:user-id (:xt/id user-doc)
-                                                :user-name (:user/name user-doc)})))
-                    ((:leave login-page) ctx)))))})
+              ((:leave register-page) (assoc ctx :error "username taken"))
+              (if-not (= (form "password") (form "password-again"))
+                ((:leave register-page) (assoc ctx :error "passwords don't match"))
+                
+                (do (let [node (-> ctx :request :node)]
+                      (xt/await-tx node
+                                   (xt/submit-tx
+                                    node
+                                    [[::xt/put {:xt/id (str (java.util.UUID/randomUUID))
+                                                :type :user
+                                                :user/name (form "username")
+                                                :user/password-hash (hashing/hash-pw (form "password"))}]])))
+                    ((:leave login-done) ctx)))))})
 
 (defn users-page [req]
   {:status 200
