c56993e2603cf405915d9d8174e7a77992c40c5b
tommy <thmorriss@gmail.com>
2022-09-03 17:44:34 -0400

    added persistent storage

--- src/frontdsl/todopage.cljs
+++ src/frontdsl/todopage.cljs
@@ -7,9 +7,11 @@
             [frontdsl.page :as page]
             
             [cljs.core.async :refer [<!]]
-            [cljs-http.client :as http]))
+            [cljs-http.client :as http]
+            [alandipert.storage-atom :refer [local-storage]]))
 
 (def todos (r/atom {}))
+(def collapsed (local-storage (r/atom {}) :collapsed))
 
 (def count-tree)
 (defn count-tree [todos]
@@ -22,35 +24,50 @@
 
 (count-tree @todos)
 (defn todo [m]
-  [:pre (pr-str m)])
+  [:pre {:key (str (:name m)
+                   (:file m))}
+   (pr-str m)])
 
-(defn todo-nest [k m]
-  (let [collapsed (r/atom true)]
-    (fn [k m]
-      [:div
+(defn todo-nest [title path m]
+  (def path path)
+  (def title title)
+  
+  (if (nil? (get-in @collapsed path))
+    (swap! collapsed assoc-in path {}))
+  
+  (let [collapsed (r/cursor collapsed path)]
+    (fn [title path m]
+      [:div {:key title}
        [:div {:style {:display "flex"}}
         [:div {:style {:padding-right "10px"
                        :cursor "pointer"}
-               :on-click #(swap! collapsed not)}
-         (if @collapsed
+               :on-click #(reset! collapsed (into {}
+                                                  (cond
+                                                    (map? m)
+                                                    (for [[k v] m]
+                                                          [k {}])
+                                                    
+                                                    (seq? m)
+                                                    [[:not-empty :not-empty]])) )}
+         (if (empty? @collapsed)
            "[+]"
            "[-]")]
         
-        [:pre (str k " (" (count-tree m) ")")]]
-       (when (not @collapsed)
+        [:pre (str title " (" (count-tree m) ")")]]
+       (when (not (empty? @collapsed))
          [:<>
           [:div {:style {:margin-left "30px"}}
            (cond
              (map? m)
              (doall (for [[k v] m]
-                      [todo-nest k v]))
+                      [todo-nest k (conj path k) v]))
              
              (seq? m)
              (map todo m))]])])))
 
 (defn tdsl-app [_]
   [:div {:style {:padding-top "30px"}}
-   [todo-nest "todos" @todos]
+   [todo-nest "todos" ["todos"] @todos]
    [page/editbox]])
 
 (defn ^:export run [inp]
